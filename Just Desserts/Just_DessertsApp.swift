3469a71a826bcafdeef4a56da5f7e105d2945b22
Mark Granoff <mark@granoff.net>
2024-06-10 09:10:14 -0400

    Initial Commit

---
+++ Just Desserts/Just_DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_DessertsApp.swift
+//  Just Desserts
+//
+//  Created by Mark Granoff on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Just_DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
